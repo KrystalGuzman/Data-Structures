60207f5396cb67386b705c931645db1f9fe4fd6a
Krystal Guzman <kryguzman@gmail.com>
2020-06-08 13:38:40 -0700

    finished stack with linked list

--- stack/stack.py
+++ stack/stack.py
@@ -8,18 +8,47 @@ return elements in Last In First Out order.
    as the underlying storage structure.
    Make sure the Stack tests pass.
 3. What is the difference between using an array vs. a linked list when 
-   implementing a Stack?
+   implementing a Stack? pop is built in vs adding and removing from tail
 """
-class Stack:
+from singly_linked_list import LinkedList
+
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+#         return self.size
+
+#     def push(self, value):
+#         self.storage.append(value)
+#         self.size += 1
+
+#     def pop(self):
+
+#         if self.size > 0:
+#             self.size -= 1
+#             return self.storage.pop()
+
+class StackFromLinkedList:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = LinkedList()
 
     def __len__(self):
-        pass
+        return self.size
 
+    # new items will be appended at head of existing list for easy access to the most recently added element
     def push(self, value):
-        pass
+        self.storage.add_to_tail(value)
+        self.size += 1
 
     def pop(self):
-        pass
+
+        if self.size > 0:
+            self.size -= 1
+            return self.storage.remove_tail()
+
+class Stack(StackFromLinkedList):
+    def __init__(self):
+        super().__init__()
